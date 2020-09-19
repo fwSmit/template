8ca7ffe179e5cc24e4dd07983a1faa258b7acf5d
fwsmit <fw.smit01@gmail.com>
2020-09-19 15:31:25 +0200

    fixed this script to be more compatible

--- createProject.sh
+++ createProject.sh
@@ -10,27 +10,27 @@ name=$1
 # echo "this script assumes you have set up a github repo"
 gitRepo="https://github.com/fwSmit/$name"
 # echo "The github repo location is $gitRepo.git"
-git clone $gitRepo ~/git_repos/$name
+git clone "$gitRepo" "$gitrepos/$name"
 if [ "$?" -ne "0" ]; then
   echo "repository $name doesn't exist"
   exit 1
 fi
-newProjectLocation="$HOME/git_repos/$name"
+newProjectLocation="$gitrepos/$name"
 echo "Cloned repo in $newProjectLocation"
-templateDir="$HOME/git_repos/template"
-cp $templateDir/cmake $templateDir/compile_commands.json $templateDir/src $templateDir/.gitignore $newProjectLocation -r
-mkdir $newProjectLocation/build
+templateDir="$gitrepos/template"
+cp "$templateDir/cmake" "$templateDir/compile_commands.json" "$templateDir/src" "$templateDir/.gitignore" "$newProjectLocation" -r
+mkdir "$newProjectLocation/build"
 while true; do
 	read -p "Do you wish to create a resources directory? (y/n) " yn
     case $yn in
-        [Yy]* ) mkdir $newProjectLocation/resources; break;;
+        [Yy]* ) mkdir "$newProjectLocation/resources"; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
-sed "s/template/$name/g" $templateDir/CMakeLists.txt > $newProjectLocation/CMakeLists.txt
+sed "s/template/$name/g" "$templateDir/CMakeLists.txt" > "$newProjectLocation/CMakeLists.txt"
 echo "Done copying the template in the project directory"
-cd $newProjectLocation
+cd "$newProjectLocation"
 git add *
 git commit -am "created project from template"
 git pull
